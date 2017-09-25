4d2c64ca09fcbcf973e192c25bf323459f8b60c7
Yusuke Yamamoto <yusuke@mac.com>
2017-09-25 16:50:03 +0900

    k

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        for (int k = 0; k < 100; k++) {
+        try{
+            System.out.println("Hello World");
+            Thread.sleep(1000);
             for (int i = 0; i < 100; i++) {
-                for (int j = 0; j < 100; j++) {
-                    System.out.println(i + j + k);
-                }
+                System.out.println(i);
             }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
