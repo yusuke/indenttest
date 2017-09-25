5365983867612ba329a14527f6c664b6a2a55087
Yusuke Yamamoto <yusuke@mac.com>
2017-09-25 16:46:58 +0900

    j

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 100; i++) {
-            System.out.println(i);
+            for (int j = 0; j < 100; j++) {
+                System.out.println(i + j);
+            }
         }
     }
 }
